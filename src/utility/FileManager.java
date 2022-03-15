ea517014dacf2dd0241a44121dcbe5989f93244a
Rodion <99991088+RodionGolovinsky@users.noreply.github.com>
2022-03-15 16:30:03 +0300

    R7

--- src/utility/FileManager.java
+++ src/utility/FileManager.java
@@ -1,8 +1,14 @@
 package utility;
 
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.annotation.*;
+
+
 public class FileManager {
-    private Xml gson = new Gson();
+    private JAXB xml = new JAXB();
     private String envVariable;
+    @XmlType(name= "Student");
 
     public FileManager(String envVariable) {
         this.envVariable = envVariable;
