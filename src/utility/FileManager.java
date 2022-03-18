b42ba1f25b79f11347ab24d7f8bed04a0ac52864
Lena <shcherba.elena@icloud.com>
2022-03-18 08:57:08 +0300

    xml 1.0

--- src/utility/FileManager.java
+++ src/utility/FileManager.java
@@ -1,16 +1,28 @@
 package utility;
 
-import javax.xml.bind.JAXB;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.annotation.*;
-
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.IOException;
 
 public class FileManager {
-    private JAXB xml = new JAXB();
+    private File file;
     private String envVariable;
-    @XmlType(name= "Student");
 
-    public FileManager(String envVariable) {
+    public FileManager(File file, String envVariable) {
         this.envVariable = envVariable;
+        this.file = file;
     }
-}
+    //нужно сверить две лабы и вытащить где xml нужное и писать дальше по первой
+ //   public void callCommand() throws IOException, JAXBException, ClassNotFoundException {
+   //     collection = new XmlParser().parseCollection(file);
+    //    Scanner sc = new Scanner(System.in);
+     //   String commands;
+      //  System.out.println("vvedite comandy");
+       // while ((commands = sc.nextLine()) != "exit") {
+
+          //  String result = stringToObject.getObjectCommand(commands).execute(collection);
+           // System.out.println(result);
+        }
+
+
