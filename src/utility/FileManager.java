c345fd989127a3a9ad8494f3bd4f0ce05e497942
Rodion <99991088+RodionGolovinsky@users.noreply.github.com>
2022-03-28 21:33:23 +0300

    я пытался

--- src/utility/FileManager.java
+++ src/utility/FileManager.java
@@ -1,5 +1,11 @@
 package utility;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 
 public class FileManager {
@@ -10,40 +16,47 @@ public class FileManager {
         this.envVariable = envVariable;
         this.file = file;
     }
-    //нужно сверить две лабы и вытащить где xml нужное и писать дальше по первой
- //   public void callCommand() throws IOException, JAXBException, ClassNotFoundException {
-   //     collection = new XmlParser().parseCollection(file);
-    //    Scanner sc = new Scanner(System.in);
-     //   String commands;
-      //  System.out.println("vvedite comandy");
-       // while ((commands = sc.nextLine()) != "exit") {
+    /*нужно сверить две лабы и вытащить где xml нужное и писать дальше по первой
+    public void callCommand() throws IOException, JAXBException, ClassNotFoundException {
+        collection = new XmlParser().parseCollection(file);
+        Scanner sc = new Scanner(System.in);
+        String commands;
+        System.out.println("vvedite comandy");
+        while ((commands = sc.nextLine()) != "exit") {
 
-          //  String result = stringToObject.getObjectCommand(commands).execute(collection);
-           // System.out.println(result);
+            String result = stringToObject.getObjectCommand(commands).execute(collection);
+            System.out.println(result);
 
-    public static class FileManager {
-        private File file;
-        private String envVariable;
-        private Cat cat;
+     */
 
-        public FileManager(File file, String envVariable) {
-            this.envVariable = envVariable;
-            this.file = file;
-        }
+//    public static class FileManager {
+//        private File file;
+//        private String envVariable;
+//
+//
+//        public FileManager(File file, String envVariable) {
+//            this.envVariable = envVariable;
+//            this.file = file;
+//        }
+//
+//        public FileManager(File file) {
+//            this.file = file;
+//        }
 
-        public FileManager(File file, Cat cat) {
-            this.file = file;
-            this.cat = cat;
-        }
+        public String create() throws ParserConfigurationException {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+            Element rootEl = document.createElement("people");
+            return null;
+            //Element
 
-        public String create() {
-            CollectionXML col = new CollectionXML(cat);
-            VehicleCollection.collection.forEach((k,v) -> col.add(k, new VehicleXML(v)));
-            Serializer serializer = new Persister();
-            serializer.write(col, out);
-            return "filepath";
+//            CollectionXML col = new CollectionXML(cat);
+//            VehicleCollection.collection.forEach((k,v) -> col.add(k, new VehicleXML(v)));
+//            Serializer serializer = new Persister();
+//            serializer.write(col, out);
+//            return "filepath";
         }
     }
-}
 
 
