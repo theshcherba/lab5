5fbff64b2f59b63ce5d4458df385e59f5933a337
Lena <shcherba.elena@icloud.com>
2022-04-29 12:30:34 +0300

    main

--- src/main/Proga.java
+++ src/main/Proga.java
@@ -1,23 +1,49 @@
 package main;
 
 import classesandenums.*;
+import utility.CollectionManager;
+import utility.CommandManager;
+import utility.QuestionAboutPerson;
+import utility.parser.FromXml;
+
 import javax.xml.bind.JAXBException;
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class Proga {
     public static final String PS1 = "$ ";
     public static final String PS2 = "> ";
-    public static void main(String[] args) throws JAXBException {
-        String path;
-        try{
-            path = args[0];
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Вы не ввели путь к коллекции при запуске, используем чистую коллекцию.");
-            path = "";
+    public static void main(String[] args){//throws JAXBException {
+        Scanner userScanner = new Scanner(System.in);
+        QuestionAboutPerson questionAboutPerson = new QuestionAboutPerson(userScanner);
+
+        CollectionManager collectionManager = new CollectionManager();
+        CommandManager commandManager = new CommandManager(collectionManager,questionAboutPerson);
+
+        if (args.length > 0) {
+            FromXml parserFromXml = new FromXml();
+            parserFromXml.parser();
+            collectionManager.Collections(FromXml.getCollection());
         }
-        Person person = new Person(336493L,"Rodin",new Coordinates(343,434), LocalDateTime.now(),  45, EColor.BLUE, HColor.BLACK,Country.RUSSIA,
-                new Location(new Double(323),434,343443L,"Novo"));
-        String arguments = args[0];
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите команду");
+            String commandName = input.nextLine();
+            commandManager.execute(commandName);
+        }
+    }
+//        String path;
+//        try{
+//            path = args[0];
+//        }catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println("Вы не ввели путь к коллекции при запуске, используем чистую коллекцию.");
+//            path = "";
+//        }
+//        Person person = new Person(336493L,"Rodin",new Coordinates(343,434), LocalDateTime.now(),  45, EColor.BLUE, HColor.BLACK,Country.RUSSIA,
+//                new Location(new Double(323),434,343443L,"Novo"));
+//        String arguments = args[0];
 
 
 
